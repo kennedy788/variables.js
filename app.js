320a3b0c9dc554c8736811dc51ea9a7c70466125
kennedy788 <salvadorjohnkennedy78@gmail.com>
2024-09-11 10:00:08 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+let name = "John Kennedy Salvador";
+
+let WhatDoYouWannaBecomeInYourLife = "programmer";
+
+let gender = "male";
+
+let twitterhandle = "@Kennedy7879";
+
+console.log(name);
+console.log(WhatDoYouWannaBecomeInYourLife);
+console.log(gender);
+console.log(twitterhandle);
